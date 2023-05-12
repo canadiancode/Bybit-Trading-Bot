755d99bb3c88d6a316c36ac2b3851cbf99fb5ada
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2023-05-12 00:28:59 -0700

    Integration of Bitget code

--- app.js
+++ app.js
@@ -8,10 +8,16 @@
 // body-parser: parse incoming JSON payloads
 // axios: promised based HTTPS requests 
 // url: helps with parsing the proxy URL for setting up QuotaGuard Static IP
+// crypto: cryptography for API POST requets
+// https: make promised based https requests 
 
 // sudo -g ngrok: sets up link to remote access apps for testing 
 
 require('dotenv').config();
+
+////////////////////////////////////////////////////////
+// BYBIT CODE -- BYBIT CODE -- BYBIT CODE -- BYBIT CODE
+////////////////////////////////////////////////////////
 
 
     // GET PRICE FEED -- GET PRICE FEED -- GET PRICE FEED -- GET PRICE FEED
@@ -86,14 +92,17 @@ app.post('/webhook', (req, res) => {
   const comment = req.body.comment;
 
   if (comment === 'Long') {
-    console.log('Going Long!')
+    console.log('Going Long!');
     postLongOrderEntry();
+    postBitgetLongOrderEntry();
   } else if (comment === 'Short') {
-    console.log('Going Short!')
+    console.log('Going Short!');
     postShortOrderEntry();
+    postBitgetShortOrderEntry();
   } else {
-    console.log('Exiting Position!')
+    console.log('Exiting Position!');
     closePosition();
+    postBitgetExitOrder();
   };
 
   res.sendStatus(200);
@@ -383,3 +392,445 @@ async function closePosition() {
   await checkOpenPositions(openPositions, "GET", walletParams, "Position");
 };
 // closePosition();
+
+
+////////////////////////////////////////////////////////////
+// BITGET CODE -- BITGET CODE -- BITGET CODE -- BITGET CODE
+///////////////////////////////////////////////////////////
+
+  // PRICE DATA FEED -- PRICE DATA FEED -- PRICE DATA FEED
+
+const GitgetWebSocket = require('websocket').client;
+const wsClient = new GitgetWebSocket();
+
+let currentBitgetBitcoinPrice = 0;
+
+const subscribeToBitgetWebSocket = () => {
+  const subscriptionMessage = {
+    "op": "subscribe",
+    "args":[
+        {
+            "instType": "mc",
+            "channel": "ticker",
+            "instId": "BTCUSDT"
+        }
+    ]
+  };
+  return JSON.stringify(subscriptionMessage);
+};
+
+wsClient.on('connectFailed', (error) => {
+  console.log('Connect Error: ' + error.toString());
+});
+
+wsClient.on('connect', (connection) => {
+
+  console.log('WebSocket Client Connected');
+
+  connection.on('error', (error) => {
+    console.log("Connection Error: " + error.toString());
+  });
+
+  connection.on('close', () => {
+    console.log('WebSocket Connection Closed');
+  });
+
+  connection.on('message', (message) => {
+    if (message.type === 'utf8') {
+      const parsedMessage = JSON.parse(message.utf8Data);
+
+      let currentTime = (new Date()).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
+
+      if (parsedMessage.data && Array.isArray(parsedMessage.data) && parsedMessage.data.length > 0) {
+        currentBitgetBitcoinPrice = parsedMessage.data[0].last;
+        console.log(`Bitcoin price at ${currentTime}: ` + currentBitgetBitcoinPrice);
+      }
+    }
+  });
+
+  if (connection.connected) {
+    connection.send(subscribeToBitgetWebSocket());
+  }
+});
+const bitgetWebSocketURL = 'wss://ws.bitget.com/mix/v1/stream';
+wsClient.connect(bitgetWebSocketURL, null);
+
+
+  // BITGET ACCOUNT INFO -- BITGET ACCOUNT INFO -- BITGET ACCOUNT INFO
+
+const BitgetApiKey = process.env.BITGET_API_KEY;
+const BitgetSecret = process.env.BITGET_API_SECRET;
+const passphrase = process.env.API_PASSPHRASE;
+
+const https = require('https');
+
+let availableBitgetBalance = '0';
+const getAccountBalance = () => {
+    const timestamp = Date.now().toString();
+    const method = 'GET';
+    const path = '/api/mix/v1/account/account';
+    const queryParams = 'marginCoin=USDT&symbol=BTCUSDT_UMCBL';
+    const baseURL = 'https://api.bitget.com';
+  
+    const signData = timestamp + method + path + '?' + queryParams;
+    const signature = crypto
+      .createHmac('sha256', BitgetSecret)
+      .update(signData)
+      .digest()
+      .toString('base64');
+  
+    const headers = {
+      'Content-Type': 'application/json',
+      'ACCESS-KEY': BitgetApiKey,
+      'ACCESS-SIGN': signature,
+      'ACCESS-TIMESTAMP': timestamp,
+      'ACCESS-PASSPHRASE': passphrase,
+    };
+  
+    const options = {
+      hostname: 'api.bitget.com',
+      path: path + '?' + queryParams,
+      method: method,
+      headers: headers,
+    };
+  
+    https.request(options, (res) => {
+
+        let data = '';
+    
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+    
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(data);
+            availableBitgetBalance = parsedData.data.available;
+            console.log('Available Balance in USDT:', availableBitgetBalance);
+          } catch (error) {
+            console.error('Error parsing response:', error.message);
+          }
+        });
+    
+    }).on('error', (error) => {
+        console.error('Error fetching account balance:', error.message);
+    }).end();
+};
+// getAccountBalance(); 
+
+let openBitgetPositions = [];
+// get open positions
+async function checkBitgetOpenPositions() {
+
+  openBitgetPositions = [];
+
+  const timestamp = Date.now().toString();
+  const method = 'GET';
+  const path = '/api/mix/v1/position/allPosition';
+  const baseURL = 'https://api.bitget.com';
+
+  const queryParams = 'productType=umcbl&marginCoin=USDT';
+  const signData = timestamp + method + path + '?' + queryParams;
+  const signature = crypto
+    .createHmac('sha256', BitgetSecret)
+    .update(signData)
+    .digest()
+    .toString('base64');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'ACCESS-KEY': BitgetApiKey,
+    'ACCESS-SIGN': signature,
+    'ACCESS-TIMESTAMP': timestamp,
+    'ACCESS-PASSPHRASE': passphrase,
+  };
+
+  const options = {
+    hostname: 'api.bitget.com',
+    path: path + '?' + queryParams,
+    method: method,
+    headers: headers,
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          const response = JSON.parse(data);
+          response.data.forEach(data => {
+            openBitgetPositions.push(data.total);
+          });
+          console.log(`Open positions: ${openBitgetPositions}`);
+
+          resolve(response);
+        } catch (error) {
+          reject(new Error('Error parsing response: ' + error.message));
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(new Error('Error getting open positions: ' + error.message));
+    });
+
+    req.end();
+  });
+};
+// checkBitgetOpenPositions();
+
+// Fetch order number
+let trackingNumber = '';
+function getTrackingNumber() {
+  const timestamp = Date.now().toString();
+  const method = 'GET';
+  const path = '/api/mix/v1/trace/currentTrack';
+  const queryParams = 'symbol=BTCUSDT_UMCBL&productType=umcbl';
+  const baseURL = 'https://api.bitget.com';
+
+  const signData = timestamp + method + path + '?' + queryParams;
+  const signature = crypto
+    .createHmac('sha256', BitgetSecret)
+    .update(signData)
+    .digest()
+    .toString('base64');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'ACCESS-KEY': BitgetApiKey,
+    'ACCESS-SIGN': signature,
+    'ACCESS-TIMESTAMP': timestamp,
+    'ACCESS-PASSPHRASE': passphrase,
+  };
+
+  const options = {
+    hostname: 'api.bitget.com',
+    path: path + '?' + queryParams,
+    method: method,
+    headers: headers,
+  };
+
+  https.request(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      try {
+        const response = JSON.parse(data);
+        if (response.code === '00000' && response.data) {
+          console.log(response.data);
+          trackingNumber = response.data[0].trackingNo;
+          console.log(`Tracking number: ${trackingNumber}`)
+        } else {
+          console.error('Error fetching current positions:', response);
+        }
+      } catch (error) {
+        console.error('Error parsing response:', error.message);
+      }
+    });
+  })
+  .on('error', (error) => {
+    console.error('Error fetching current positions:', error.message);
+  })
+  .end();
+};
+// getTrackingNumber();
+
+
+  // ORDER FUNCTIONS -- ORDER FUNCTIONS -- ORDER FUNCTIONS
+
+// open order
+function createOrder(direction, positionSize, clientOid) {
+  const timestamp = Date.now().toString();
+  const method = 'POST';
+  const path = '/api/mix/v1/order/placeOrder';
+  const baseURL = 'https://api.bitget.com';
+
+  const requestBody = JSON.stringify({
+    symbol: 'BTCUSDT_UMCBL',
+    marginCoin: 'USDT',
+    size: positionSize, // 0.01
+    price: currentBitgetBitcoinPrice,
+    side: direction, // open_long
+    orderType: 'market', // limit
+    timeInForceValue: 'normal',
+    clientOid: clientOid,
+  });
+
+  const signData = timestamp + method + path + requestBody;
+  const signature = crypto
+    .createHmac('sha256', BitgetSecret)
+    .update(signData)
+    .digest()
+    .toString('base64');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'ACCESS-KEY': BitgetApiKey,
+    'ACCESS-SIGN': signature,
+    'ACCESS-TIMESTAMP': timestamp,
+    'ACCESS-PASSPHRASE': passphrase,
+  };
+
+  const options = {
+    hostname: 'api.bitget.com',
+    path: path,
+    method: method,
+    headers: headers,
+  };
+
+  const req = https.request(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      try {
+        console.log(JSON.parse(data));
+      } catch (error) {
+        console.error('Error parsing response:', error.message);
+      }
+    });
+  });
+
+  req.on('error', (error) => {
+    console.error('Error creating order:', error.message);
+  });
+
+  req.write(requestBody);
+  req.end();
+};
+
+// close order
+async function closeBitgetPosition(trackingNo) {
+  const timestamp = Date.now().toString();
+  const method = 'POST';
+  const path = '/api/mix/v1/trace/closeTrackOrder';
+  const baseURL = 'https://api.bitget.com';
+
+  const requestBody = {
+    symbol: 'BTCUSDT_UMCBL',
+    trackingNo: trackingNo,
+  };
+
+  const signData = timestamp + method + path + JSON.stringify(requestBody);
+  const signature = crypto
+    .createHmac('sha256', BitgetSecret)
+    .update(signData)
+    .digest()
+    .toString('base64');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'ACCESS-KEY': BitgetApiKey,
+    'ACCESS-SIGN': signature,
+    'ACCESS-TIMESTAMP': timestamp,
+    'ACCESS-PASSPHRASE': passphrase,
+  };
+
+  const options = {
+    hostname: 'api.bitget.com',
+    path: path,
+    method: method,
+    headers: headers,
+  };
+
+  const req = https.request(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      try {
+        console.log(JSON.parse(data));
+      } catch (error) {
+        console.error('Error parsing response:', error.message);
+      }
+    });
+  });
+
+  req.on('error', (error) => {
+    console.error('Error closing trader position:', error.message);
+  });
+
+  req.write(JSON.stringify(requestBody));
+  req.end();
+};
+
+
+  // LONG, SHORT, EXIT FUNCTIONS -- LONG, SHORT, EXIT FUNCTIONS
+
+let clientOid = '';
+let BitgetLeverage = 1;
+let BitgetPositionSize = 0;
+
+async function postBitgetLongOrderEntry() {
+  tradeDirection = 'long';
+
+  await getAccountBalance();
+
+  BitgetPositionSize = availableBitgetBalance * BitgetLeverage;
+  console.log(`Actual position size would be ${BitgetPositionSize}`);
+
+  const generateClientOid = () => {
+    const prefix = 'myapp';
+    const timestamp = Date.now();
+    const randomPart = Math.floor(Math.random() * 1e6);
+    return `${prefix}-${timestamp}-${randomPart}`;
+  };
+
+  clientOid = generateClientOid();
+
+  await createOrder('open_long', BitgetPositionSize, clientOid) // direction, BitgetPositionSize, clientOid
+};
+// setTimeout(postBitgetLongOrderEntry, 5000);
+
+async function postBitgetShortOrderEntry() {
+  tradeDirection = 'short';
+
+  await getAccountBalance();
+
+  BitgetPositionSize = availableBitgetBalance * BitgetLeverage;
+  console.log(`Actual position size would be ${BitgetPositionSize}`);
+
+  const generateClientOid = () => {
+    const prefix = 'myapp';
+    const timestamp = Date.now();
+    const randomPart = Math.floor(Math.random() * 1e6);
+    return `${prefix}-${timestamp}-${randomPart}`;
+  };
+
+  clientOid = generateClientOid();
+
+  await createOrder('open_short', BitgetPositionSize, clientOid)  // direction, BitgetPositionSize, clientOid
+};
+// setTimeout(postBitgetShortOrderEntry, 5000);
+
+
+async function postBitgetExitOrder() {
+
+  await getTrackingNumber();
+    
+  await closeBitgetPosition(trackingNumber);
+
+  await checkBitgetOpenPositions();
+
+  if (openBitgetPositions[0] === '0' && openBitgetPositions[1] === '0') {
+    console.log('All positions closed.');
+  } else {
+    postBitgetExitOrder();
+  }
+};
+// setTimeout(postBitgetExitOrder, 10000);
